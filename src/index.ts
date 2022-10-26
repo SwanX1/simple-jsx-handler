a89b997576e6ff14a339ab9cc988b91dcd8ccd5a
Kārlis Čerņavskis <karliscern@gmail.com>
2022-10-26 21:44:52 +0300

    apparently jsx doesn't work that way

--- src/index.ts
+++ src/index.ts
@@ -10,18 +10,20 @@ declare global {
 
 type removeOn<T extends `${'on' | 'once'}:${string}`> = T extends `${'on' | 'once'}:${infer Rest}` ? Rest : never;
 
-type ElementAttributes = {
+export type ElementAttributes = {
   [K in `${'on' | 'once'}:${keyof HTMLElementEventMap}`]?: (
     this: HTMLElement,
     event: HTMLElementEventMap[removeOn<K>]
   ) => void;
 } & {
-  [key: string]: string;
+  [key: string]: unknown;
 };
+
+export type Renderable<T extends HTMLElement = HTMLElement> = (attributes: ElementAttributes, ...children: Node[]) => T;
 
 export const _fragment = Symbol('_fragment');
 
-export interface RenderableOptions<T extends HTMLElement = HTMLElement> {
+interface RenderableOptions<T extends HTMLElement = HTMLElement> {
   attributes: Record<string, unknown>;
   events: {
     on: { [K in keyof HTMLElementEventMap]?: (this: T, event: HTMLElementEventMap[K]) => void };
@@ -29,12 +31,12 @@ export interface RenderableOptions<T extends HTMLElement = HTMLElement> {
   };
 }
 
-export interface Renderable {
-  render(options: RenderableOptions, ...children: Node[]): HTMLElement;
-}
-
 export function _createElement(tag: symbol, props: ElementAttributes, ...children: Node[]): HTMLElement;
-export function _createElement(tag: Renderable, props: ElementAttributes, ...children: Node[]): HTMLElement;
+export function _createElement<T extends HTMLElement>(
+  tag: (attrs: ElementAttributes, ...children: Node[]) => T,
+  props: ElementAttributes,
+  ...children: Node[]
+): T;
 export function _createElement<TagName extends keyof JSX.IntrinsicElements>(
   tag: TagName,
   props: ElementAttributes,
@@ -51,7 +53,7 @@ export function _createElement<TagName extends keyof HTMLElementDeprecatedTagNam
   ...children: Node[]
 ): HTMLElementDeprecatedTagNameMap[TagName];
 export function _createElement(
-  tag: string | symbol | Renderable,
+  tag: string | symbol | ((attrs: ElementAttributes, ...children: Node[]) => HTMLElement),
   attrs: ElementAttributes,
   ...children: Node[]
 ): HTMLElement {
@@ -60,6 +62,10 @@ export function _createElement(
       throw new TypeError('Invalid symbol for _createElement function!');
     }
     tag = 'jsx-frag';
+  }
+
+  if (typeof tag === 'function') {
+    return tag(attrs, ...children);
   }
 
   const renderableOptions: RenderableOptions = {
@@ -89,51 +95,46 @@ export function _createElement(
     }
   }
 
-  let element: HTMLElement;
-  if (typeof tag === 'object' && 'render' in tag) {
-    element = tag.render(renderableOptions, ...children);
-  } else {
-    if (typeof tag !== 'string') {
-      throw new TypeError('Tag is not a string.', {
-        cause: {
-          context: {
-            tag,
-            attributes: renderableOptions.attributes,
-            events: {
-              on: renderableOptions.events.on,
-              once: renderableOptions.events.once,
-            },
+  if (typeof tag !== 'string') {
+    throw new TypeError('Tag is not a string.', {
+      cause: {
+        context: {
+          tag,
+          attributes: renderableOptions.attributes,
+          events: {
+            on: renderableOptions.events.on,
+            once: renderableOptions.events.once,
           },
         },
+      },
+    });
+  }
+  const element = document.createElement(tag);
+
+  for (const [eventName, value] of Object.entries(renderableOptions.events.once)) {
+    if (checkEvent(eventName, value)) {
+      element.addEventListener(eventName, function listener(e) {
+        element.removeEventListener(eventName, listener);
+        // @ts-expect-error ...
+        value.call(element, e);
       });
     }
-    element = document.createElement(tag);
+  }
 
-    for (const [eventName, value] of Object.entries(renderableOptions.events.once)) {
-      if (checkEvent(eventName, value)) {
-        element.addEventListener(eventName, function listener(e) {
-          element.removeEventListener(eventName, listener);
-          // @ts-expect-error ...
-          value.call(element, e);
-        });
-      }
+  for (const [eventName, value] of Object.entries(renderableOptions.events.on)) {
+    if (checkEvent(eventName, value)) {
+      // @ts-expect-error ...
+      element.addEventListener(eventName, value.bind(element));
     }
+  }
 
-    for (const [eventName, value] of Object.entries(renderableOptions.events.on)) {
-      if (checkEvent(eventName, value)) {
-        // @ts-expect-error ...
-        element.addEventListener(eventName, value.bind(element));
-      }
-    }
+  for (const [attribute, value] of Object.entries(renderableOptions.attributes)) {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    element.setAttribute(attribute, (value as any).toString());
+  }
 
-    for (const [attribute, value] of Object.entries(renderableOptions.attributes)) {
-      // eslint-disable-next-line @typescript-eslint/no-explicit-any
-      element.setAttribute(attribute, (value as any).toString());
-    }
-
-    for (const child of children) {
-      element.appendChild(typeof child.nodeType === 'undefined' ? document.createTextNode(child.toString()) : child);
-    }
+  for (const child of children) {
+    element.appendChild(typeof child.nodeType === 'undefined' ? document.createTextNode(child.toString()) : child);
   }
 
   return element;
